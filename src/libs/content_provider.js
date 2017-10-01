86bb6a2d070e88789f35872f325000613aa118ea
carlosjln <carlosjln01@gmail.com>
2017-09-30 21:53:07 -0400

    fixed and improved content provider get_template plus unit test

--- src/libs/content_provider.js
+++ src/libs/content_provider.js
@@ -26,36 +26,40 @@ let types_filters = {
 	'components': match_js_file
 };
 
-function content_provider( config ) {
-	this.module_path = config.module_path;
-	this.cache = config.cache;
-	this.cache_storage = {
-		templates: {},
-		components: {}
-	};
-
-	this.update_cache();
-}
-
-function get_templates( base_path, file_names ) {
+/**
+ * Returns a list of template objects corresponding to the specified module.
+ * @param {String} module_path - full path of the module containing the templates
+ * @param {String[]} [file_names] - list of file names that you want to look for.
+ */
+function get_templates( module_path, file_names ) {
 	let templates = [];
 
-	if( file_names && file_names.length ) {
-		let raw_name;
-		let filepath;
-
-		for( let i = 0; i < file_names.length; i++ ) {
-			raw_name = file_names[ i ].replace( match_file_extension, '' );
-			filepath = Path.join( base_path, raw_name );
-
-			templates.add( {
-				id: raw_name,
-				html: IO.get_content( filepath + '.html' ),
-				style: IO.get_content( filepath + '.css' )
-			} );
-		}
+	if( !file_names || !file_names.length ) {
+		return templates;
 	}
 
+	let done = {};
+	let raw_name;
+	let filepath;
+
+	for( let i = 0; i < file_names.length; i++ ) {
+		// remove the file extension so that only unique file names are processed
+		raw_name = file_names[ i ].replace( match_file_extension, '' );
+
+		if( done[ raw_name ] ) {
+			continue;
+		}
+
+		done[ raw_name ] = true;
+		filepath = Path.join( module_path, raw_name );
+
+		templates.add( {
+			id: raw_name,
+			html: IO.get_content( filepath + '.html' ),
+			style: IO.get_content( filepath + '.css' )
+		} );
+	}
+	
 	return templates;
 }
 
@@ -79,6 +83,21 @@ function get_components( base_path, file_names ) {
 	}
 
 	return components;
+}
+
+function content_provider( config ) {
+	var module_path = this.module_path = config.module_path;
+	this.cache = config.cache;
+	this.cache_storage = {
+		templates: {},
+		components: {}
+	};
+
+	this.main_js_path = Path.join( module_path, 'main.js' );
+	this.templates_path = Path.join( module_path, "templates" );
+	this.components_path = Path.join( module_path, "components" );
+
+	this.update_cache();
 }
 
 content_provider.prototype = {
@@ -106,28 +125,21 @@ content_provider.prototype = {
 	},
 
 	get_main: function () {
-		let filepath = Path.join( this.module_path, 'main.js' );
-		return this.cache_storage.main || IO.get_content( filepath );
+		return this.cache_storage.main || IO.get_content( this.main_js_path );
 	},
 
 	get_templates: function ( names ) {
-		let base_path = Path.join( this.module_path, "templates" );
+		let templates_path = this.templates_path;
+		let file_names = names ? names : IO.get_files( templates_path );
 
-		if( names && names.length ) {
-			return get_templates( base_path, names );
-		}
-
-		return get_templates( base_path, IO.get_files( base_path ).unique() );
+		return get_templates( templates_path, file_names );
 	},
 
 	get_components: function ( names ) {
-		let base_path = Path.join( this.module_path, "components" );
+		let components_path = Path.join( this.module_path, "components" );
+		let file_names = names ? names : IO.get_files( components_path );
 
-		if( names && names.length ) {
-			return get_templates( base_path, names );
-		}
-
-		return get_templates( base_path, IO.get_files( base_path ).unique() );
+		return get_templates( components_path, file_names );
 	}
 };
 
--- test/unit/test-content_provider.js
+++ test/unit/test-content_provider.js
@@ -15,52 +15,33 @@ Test.beforeEach( t => {
 
 Test( 'should have the specified module path', t => {
 	let content_provider = t.context;
-
+	
 	t.is( content_provider.module_path, expected_path );
 } );
 
 Test( 'get_main returns the content of /test-module/main.js', t => {
 	let content_provider = t.context;
 	let main_js_content = content_provider.get_main();
-
+	
 	t.is( main_js_content, "alert('boo');" );
 } );
 
-Test( 'get_templates returns html & css from the /test-module/template/main.(html|css) files.', t => {
+Test( 'get_templates returns an object containing properties id, html & css that match their file content.', t => {
 	let content_provider = t.context;
+	
+	var expected_main = {
+		id: 'main',
+		html: '<html>main template</html>',
+		style: '.main-style {}'
+	};
+	let expected_template = [ expected_main ];
+
 	let template = content_provider.get_templates();
-	let expected_template = [ {
-		html: '',
-		css: ''
-	} ];
-	t.deepEqual( template, expected_template );
+
+	// t.snapshot( template, 'template' );
+	t.not( template, null );
+
+	t.is( template.length, 1 );
+
+	t.deepEqual( template[0], expected_main );
 } );
-
-// 	
-// test( 'test in progress', t => {
-// 	t.true( 1 === 2 );
-// } );
-// test( 'objects', t => {
-// 	const actual = {
-// 		a: 1,
-// 		b: {
-// 			c: 2
-// 		}
-// 	};
-
-// 	const expected = {
-// 		a: 2,
-// 		b: {
-// 			c: 1
-// 		}
-// 	};
-
-// 	t.deepEqual( actual, expected );
-// } );
-// test('test in progress', t => {
-// 	t.true(assert.equal( '', __dirname ));
-// });
-
-// test( 'BBB', t => {
-// 	t.truthy( 'a' === __dirname );
-// } );
