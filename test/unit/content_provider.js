53279ba98b791a1de4bf4e0cc5d64c4b91aa3b97
carlosjln <carlosjln01@gmail.com>
2018-06-05 16:50:42 -0400

    simplified variables

--- test/unit/content_provider.js
+++ test/unit/content_provider.js
@@ -4,7 +4,7 @@ import Test from 'ava';
 
 import Path from 'path';
 import ContentProvider from '../../src/libs/content_provider';
-import sinon from 'sinon';
+import Sinon from 'sinon';
 
 let real_module_path = Path.join( 'real', 'module' );
 let fake_module_path = Path.join( 'fake', 'module' );
@@ -17,12 +17,12 @@ Test.before( t => {
 
 	let real_template_main_html = Path.join( real_templates_path, 'main.html' );
 	let real_template_main_css = Path.join( real_templates_path, 'main.css' );
-	
+
 	let real_template_one_html = Path.join( real_templates_path, 'one.html' );
 	let real_template_one_css = Path.join( real_templates_path, 'one.css' );
 
-	let existsSync = sinon.stub( FS, 'existsSync' );
-	let readFileSync = sinon.stub( FS, 'readFileSync' );
+	let existsSync = Sinon.stub( FS, 'existsSync' );
+	let readFileSync = Sinon.stub( FS, 'readFileSync' );
 
 	existsSync.withArgs( real_templates_path ).returns( true );
 	existsSync.withArgs( fake_templates_path ).returns( false );
@@ -38,38 +38,41 @@ Test.before( t => {
 } );
 
 Test( 'should have the specified module path', t => {
-	let config = { module_path: real_module_path };
-	let content_provider = new ContentProvider( config );
+	let content_provider = new ContentProvider( {
+		module_path: real_module_path
+	} );
 
 	t.is( content_provider.module_path, real_module_path );
 } );
 
 Test( 'get_main() should return the content of the module\'s main.js file', t => {
-	let config = { module_path: real_module_path };
-	let content_provider = new ContentProvider( config );
+	let content_provider = new ContentProvider( {
+		module_path: real_module_path
+	} );
 	let content = content_provider.get_main();
 
 	t.is( content, "alert('madness');" );
 } );
 
 Test( 'get_templates( [names] ) should return a list of template objects that match the specified template names.', t => {
-	let config = { module_path: real_module_path };
-	let content_provider = new ContentProvider( config );
+	let content_provider = new ContentProvider( {
+		module_path: real_module_path
+	} );
 
-	var main_template = {
+	let main_template = {
 		id: 'main',
 		html: '<html>main</html>',
 		style: '.main-style{}'
 	};
 
-	var one_template = {
+	let one_template = {
 		id: 'one',
 		html: '<html>one</html>',
 		style: '.one-style{}'
 	};
 
 	// repeated 'main' to test unique name filtering
-	var template_names = [ 'main', 'one', 'main' ];
+	let template_names = [ 'main', 'one', 'main' ];
 	let templates = content_provider.get_templates( template_names );
 
 	// must return empty list if templates directory is empty or does not exist
@@ -83,9 +86,9 @@ Test( 'get_templates( [names] ) should return a list of template objects that ma
 } );
 
 Test( 'get_templates([]) should return an empty list when not receiving file names to look up.', t => {
-	let config = { module_path: real_module_path };
-	let content_provider = new ContentProvider( config );
-
+	let content_provider = new ContentProvider( {
+		module_path: real_module_path
+	} );
 	let templates = content_provider.get_templates();
 
 	// must return a list
@@ -96,8 +99,9 @@ Test( 'get_templates([]) should return an empty list when not receiving file nam
 } );
 
 Test( 'get_templates() should return an empty list when the provided templates path does not exist.', t => {
-	let config = { module_path: fake_module_path };
-	let content_provider = new ContentProvider( config );
+	let content_provider = new ContentProvider( {
+		module_path: fake_module_path
+	} );
 	let templates = content_provider.get_templates();
 
 	// must return a list
@@ -108,8 +112,9 @@ Test( 'get_templates() should return an empty list when the provided templates p
 } );
 
 Test( 'get_components() should return an empty list when the provided component path does not exist.', t => {
-	let config = { module_path: fake_module_path };
-	let content_provider = new ContentProvider( config );
+	let content_provider = new ContentProvider( {
+		module_path: fake_module_path
+	} );
 	let components = content_provider.get_components();
 
 	// must return a list
